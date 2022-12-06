5bf615debb3a4eabbdd44920f46f3fc79978bd98
oliver <oliver.nagy510@gmail.com>
2022-12-06 20:42:07 +0100

    simulation of 1000 years

--- test.py
+++ test.py
@@ -1,16 +1,62 @@
 #recalculate G for the situation
 #change v units
+#figure out time flow
 
 import rebound
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import csv
 
 sim = rebound.Simulation()
 
-def vel(x, y):
-    return (center[1]-y, x-center[0])
-center = (0, 0)
+# unit conversion
+PC = 3.08567758E16  # PC to m
+MS = 1.989E30       # Ms to kg
+sim.G = 6.67E-11    # in m^3 kg^-1 s^-2
+sim.dt = 31556926   # timestep of 1 year
+
+
+# loading data into simulation
+with open("data.csv", "r") as fp:
+    data = csv.reader(fp)
+    m = 0
+    # counter = 0
+    for i in data:
+        # counter += 1
+        # if(counter == 10):
+        #     break
+        if(m==0):
+            m = float(i[0])
+            continue
+        sim.add(m=m*MS, x=float(i[0])*PC, y=float(i[1])*PC, z=float(i[2])*PC, vx=float(i[3])*1000, vy=float(i[4])*1000, vz=float(i[5])*1000)
+
+# integrate in time
+data = [[[i.x],[i.y],[i.z]] for i in sim.particles]
+
+# print(sim.calculate_com())
+for i in range(100):
+    sim.steps(100)
+    for j in range(len(sim.particles)):
+        data[j][0].append(sim.particles[j].x)
+        data[j][1].append(sim.particles[j].y)
+        data[j][2].append(sim.particles[j].z)
+# print(sim.status())
+# print(len(data))
+# print(sim.calculate_com())
+
+# show data afterwards
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+for i in data:
+    ax.plot3D(i[0], i[1], i[2])
+# ax.plot3D(data[500][0], data[500][1], data[500][2])
+# ax.plot3D(data[2][0], data[2][1], data[2][2])
+plt.show()
+
+# def vel(x, y):
+#     return (center[1]-y, x-center[0])
+# center = (0, 0)
 
 # center = (0.5, math.sin(math.pi/3)/2)
 # coords1 = (0, 0)
@@ -32,20 +78,20 @@ center = (0, 0)
 # xpos3 = []
 # ypos3 = []
 
-points = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
-vels = [vel(i/2, j/2) for i,j in points]
-pos = [[[], []] for i in range(len(points))]
-for i in range(len(points)):
-    sim.add(m=1, x=points[i][0], y=points[i][1], vx=vels[i][0], vy=vels[i][1])
+# points = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+# vels = [vel(i/2, j/2) for i,j in points]
+# pos = [[[], []] for i in range(len(points))]
+# for i in range(len(points)):
+#     sim.add(m=1, x=points[i][0], y=points[i][1], vx=vels[i][0], vy=vels[i][1])
 
-print(sim.status())
+# print(sim.status())
 
 
-for i in range(1, 1000):
-    sim.integrate(i/100)
-    for j in range(len(sim.particles)):
-        pos[j][0].append(sim.particles[j].x*100)
-        pos[j][1].append(sim.particles[j].y*100)
+# for i in range(1, 1000):
+#     sim.integrate(i/100)
+#     for j in range(len(sim.particles)):
+#         pos[j][0].append(sim.particles[j].x*100)
+#         pos[j][1].append(sim.particles[j].y*100)
     # for p in sim.particles:
     #     print(p.x, p.y, p.z)
     # xpos1.append(sim.particles[0].x*100)
@@ -60,14 +106,14 @@ for i in range(1, 1000):
 # for o in sim.calculate_orbits():
 #     print(o)
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 # ax.plot(xpos1, ypos1, label="first one")
 # ax.plot(xpos2, ypos2)
 # ax.plot(xpos3, ypos3)
-for i in pos:
-    ax.plot(i[0], i[1], label="x")
-ax.legend()
-plt.show()
+# for i in pos:
+#     ax.plot(i[0], i[1], label="x")
+# ax.legend()
+# plt.show()
 
 # for p in sim.particles:
 #     print(p.x, p.y, p.z)
--- test2.py
+++ test2.py
@@ -3,25 +3,21 @@
 import rebound
 import matplotlib.pyplot as plt
 import csv
-sim = rebound.Simulation()
+import numpy as np
 
-with open("data.csv", "r") as fp:
-    data = csv.reader(fp)
-    m = 0
-    for i in data:
-        if(m==0):
-            m = float(i[0])
-            print(m)
-            print(type(m))
-            continue
-        #change velocity units?
-        sim.add(m=m, x=float(i[0]), y=float(i[1]), z=float(i[2]))
 
-xdata = [i.x for i in sim.particles]
-ydata = [i.y for i in sim.particles]
-zdata = [i.z for i in sim.particles]
-
-fig = plt.figure()
-ax = plt.axes(projection="3d")
-ax.scatter3D(xdata, ydata, zdata)
+fig = plt.figure(figsize=(9, 6))
+#create data for 3d line
+xline = np.linspace(0, 15, 1000)
+yline = np.sin(xline)
+zline = np.cos(xline)
+#3d container
+ax = plt.axes(projection = '3d')
+#3d scatter plot
+ax.plot3D(xline, yline, zline)
+ax.plot3D(yline, zline, xline)
+#give labels
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
 plt.show()
